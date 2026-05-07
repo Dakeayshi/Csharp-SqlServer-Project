e3224fc509d085a33fabb7201763c8ba46ca9c68
Dakeayshi <155475425+Dakeayshi@users.noreply.github.com>
2026-05-07 18:15:25 +0800

    Support fully language switch, support languages: English, Maly and Chinese

---
+++ ConsoleConnectSqlServer/InfoHelper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleConnectSqlServer
+{
+    internal class InfoHelper
+    {
+        public static string Gender1 { get; set; }
+
+        public static string Gender2 { get; set; }
+
+        public static string Info1 { get; set; }
+
+        public static string Info2 { get; set; }
+
+        public static string Info3 { get; set; }
+
+        public static string Info4 { get; set; }
+
+        public static string Info5 { get; set; }
+
+        public static string Info6 { get; set; }
+
+        public static string Info7 { get; set; }
+
+        public static string Info8 { get; set; }
+
+        public static string Info9 { get; set; }
+
+        public static string Info10 { get; set; }
+
+        public static string Info11 { get; set; }
+
+        public static string Info12 { get; set; }
+    }
+}
--- ConsoleConnectSqlServer/Program.cs
+++ ConsoleConnectSqlServer/Program.cs
@@ -14,24 +14,68 @@ namespace ConsoleConnectSqlServer
         static void Main(string[] args)
         {
             //Choose display language
-            Console.WriteLine("1. English 2:中文");
+            Console.WriteLine("1. English 2:中文 3: Bahasa Melayu");
             string inputLang = Console.ReadLine();
-            int lang = 1;
-            if (inputLang == "2")
-                lang = 2;
 
+            if (inputLang == "2")
+            {
+                InfoHelper.Gender1 = "男";
+                InfoHelper.Gender2 = "女";
+                InfoHelper.Info1 = "欢迎登录!";
+                InfoHelper.Info2 = "请在下方输入您的用户名和密码";
+                InfoHelper.Info3 = "请输入用户名: ";
+                InfoHelper.Info4 = "请输入密码: ";
+                InfoHelper.Info6 = "欢迎, @NickName";
+                InfoHelper.Info7 = "请选择性别";
+                InfoHelper.Info8 = "选项: 1: 男性 2: 女性";
+                InfoHelper.Info9 = "@Username的性别更新失败";
+                InfoHelper.Info10 = "@Username的性别更新成功";
+                InfoHelper.Info11 = "用户名 昵称 性别";
+            }
+            else if (inputLang == "3")
+            {
+                InfoHelper.Gender1 = "Lelaki";
+                InfoHelper.Info1 = "Selamat datang log masuk!";
+                InfoHelper.Info2 = "Sila masukkan nama pengguna dan kata laluan anda di bawah";
+                InfoHelper.Info3 = "Masukkan nama pengguna anda: ";
+                InfoHelper.Info4 = "Masukkan kata laluan anda: ";
+                InfoHelper.Info5 = "Sama ada nama pengguna atau kata laluan anda salah";
+                InfoHelper.Info6 = "Selamat datang, @NickName";
+                InfoHelper.Info7 = "Please select your gender";
+                InfoHelper.Info8 = "Pilihan: 1: Lelaki 2: Perempuan";
+                InfoHelper.Info9 = "Gagal memuatkan jantina ke @Username";
+                InfoHelper.Info10 = "Berjaya mengemas kini jantina @Username";
+                InfoHelper.Info11 = "Nama Pengguna Nama Panggilan Jantina";
+            }
+            else
+            {
+                InfoHelper.Gender1 = "Male";
+                InfoHelper.Gender2 = "Female";
+                InfoHelper.Info1 = "Welcome login!";
+                InfoHelper.Info2 = "Please enter your username and password below";
+                InfoHelper.Info3 = "Enter your username: ";
+                InfoHelper.Info4 = "Enter your password: ";
+                InfoHelper.Info5 = "Either your username or password is incorrect";
+                InfoHelper.Info6 = "Welcom, @NickName";
+                InfoHelper.Info7 = "Please select your gender";
+                InfoHelper.Info8 = "Option: 1: Male 2: Female";
+                InfoHelper.Info9 = "Failed to load gender to @Username";
+                InfoHelper.Info10 = "Successed to update gender of @Username";
+                InfoHelper.Info11 = "Username Nickname Gender";
+            }
 
             //Print system infomation
-            Console.WriteLine("Welcom to XXX system!");
-            Console.WriteLine("Please enter your username and password below");
+            Console.WriteLine(InfoHelper.Info1);
+            Console.WriteLine(InfoHelper.Info2);
 
             DataTable table = null;
             while (true)
             {
-                Console.Write("Enter your username: ");
+                Console.Write(InfoHelper.Info3);
                 string inputName = Console.ReadLine();
 
-                Console.Write("Enter your password: ");
+                
+                Console.Write(InfoHelper.Info4);
                 string inputPwd = Console.ReadLine();
 
                 //Verify user's input
@@ -39,7 +83,7 @@ namespace ConsoleConnectSqlServer
                 table = SelectData(sql);
                 if (table.Rows.Count <= 0)
                 {
-                    Console.WriteLine("Either your username or password is incorrect");
+                    Console.WriteLine(InfoHelper.Info5);
                     continue;
                 }
 
@@ -47,17 +91,18 @@ namespace ConsoleConnectSqlServer
             }
 
             //Welcoming page
-            Console.WriteLine($"Welcome, {table.Rows[0]["NickName"]}");
+            InfoHelper.Info6 = InfoHelper.Info6.Replace("@NickName",table.Rows[0]["NickName"].ToString());
+            Console.WriteLine(InfoHelper.Info6);
 
             //Fetch gender info, ask untill value is not null or empty
             string gender = table.Rows[0]["Gender"].ToString();
             if (string.IsNullOrEmpty(gender))
             {
-                Console.WriteLine("Please provide your gender information");
+                Console.WriteLine(InfoHelper.Info7);
 
                 while (true)
                 {
-                    Console.WriteLine("Option: 1: Male 2: Female");
+                    Console.WriteLine(InfoHelper.Info8);
                     string inputGender = Console.ReadLine();
                     if (inputGender != "1" && inputGender != "2")
                         continue;
@@ -67,11 +112,15 @@ namespace ConsoleConnectSqlServer
                     int i = EditData(updateGenderSql);
                     if (i < 0)
                     {
-                        Console.WriteLine($"Failed to load gender to {table.Rows[0]["UserName"]}");
+                        InfoHelper.Info9 = InfoHelper.Info9.Replace("@Username", table.Rows[0]["UserName"].ToString());
+                        Console.WriteLine(InfoHelper.Info9);
+                        //Console.WriteLine($"Failed to load gender to {table.Rows[0]["UserName"]}");
                     }
                     else
                     {
-                        Console.WriteLine($"Successed to update gender of {table.Rows[0]["UserName"]}");
+                        InfoHelper.Info10 = InfoHelper.Info10.Replace("@Username", table.Rows[0]["UserName"].ToString());
+                        Console.WriteLine(InfoHelper.Info10);
+                        //Console.WriteLine($"Successed to update gender of {table.Rows[0]["UserName"]}");
                     }
                     break;
                 }
@@ -82,26 +131,16 @@ namespace ConsoleConnectSqlServer
             string sqlAllUsers = "SELECT * FROM UserT;";
             DataTable tableAllUsers = SelectData(sqlAllUsers);
 
-            Console.WriteLine("Username Nickname Gender");
+            Console.WriteLine(InfoHelper.Info11);
             for (int i = 0; i < tableAllUsers.Rows.Count; i++)
             {
                 string genderForSql = tableAllUsers.Rows[i]["Gender"].ToString();
-                if(lang==1)
-                {
-                    if (genderForSql == "1")
-                        genderForSql = "Male";
-                    else if (genderForSql == "2")
-                        genderForSql = "Female";
-                }
-                if(lang==2)
-                {
-                    if (genderForSql == "Male")
-                        genderForSql = "男";
-                    else if (genderForSql == "Female") 
-                        genderForSql = "女";
-                }
+                if (genderForSql == "1")
+                    genderForSql = InfoHelper.Gender1;
+                else if (genderForSql == "2")
+                    genderForSql = InfoHelper.Gender2; 
 
-                Console.WriteLine($"Username: {tableAllUsers.Rows[i]["UserName"]}; {tableAllUsers.Rows[i]["NickName"]}; {genderForSql}");
+                Console.WriteLine($"{tableAllUsers.Rows[i]["UserName"]}; {tableAllUsers.Rows[i]["NickName"]}; {genderForSql}");
             }
 
             Console.ReadKey();
